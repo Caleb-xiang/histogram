ec399b9ecf9e5d417281a09025045bd75dfd6471
Caleb-xiang <xiangcaleb@outlook.com>
2025-04-03 18:38:02 +0800

    Update lei.v

--- lei_a.v
+++ lei_a.v
@@ -1,43 +1,49 @@
-module histogram_equ
+module hist_equ
 #(
-    parameter  [9:0] IMG_HDISP = 10'd640,  //640*480
-    parameter  [9:0] IMG_VDISP = 10'd480
+    parameter  [9:0] IMG_HDISP = 10'd100,  //640*480
+    parameter  [9:0] IMG_VDISP = 10'd100
 )
 (
-    input       clk,
-    input       rst_n,
-    input       per_frame_vsync,
-    input       per_frame_href,
-    input       per_frame_clken,
-    input   [7:0] per_img_Y,
-    output      post_frame_vsync,
-    output      post_frame_href,
-    output      post_frame_clken,
-    output  [7:0] post_img_Y
+    input   wire            clk             ,
+    input   wire            rst_n           ,
+
+    input   wire            per_frame_vsync ,        //每帧的垂直同步信号
+    input   wire            per_frame_href  ,         //每帧的水平同步信号
+    input   wire            per_frame_de    ,        //数据有效信号
+    input   wire    [7:0]   per_img_Y       ,
+
+    output  wire            post_frame_vsync,
+    output  wire            post_frame_href ,
+    output  wire            post_frame_clken,
+    output  wire    [7:0]   post_img_Y
 );
 
-localparam IMG_TOTAL_PIXEL = 307200;  //一帧图像的总像素数
+//************** Parameter and Internal Signal **************//
+
+localparam IMG_TOTAL_PIXEL = 10000;  //一帧图像的总像素数
 localparam IMG_MAX_GRAY   = 256;     //图像中可能的灰度等级数量
 
-wire [18:0] ram_his_rd_data;
-reg        ram_his_wr_en;
-reg        vsync_delay;
+wire    [18:0]      ram_his_rd_data;
+reg                 ram_his_wr_en;
+reg                 vsync_delay;
 
-reg  [7:0] ram_his_addr_b;
-reg        ram_his_rd_b;
-wire [18:0] ram_his_rd_data_b;
+reg     [7:0]       ram_his_addr_b;
+reg                 ram_his_rd_b;
+wire    [18:0]      ram_his_rd_data_b;
 
-reg ram_his_wr_b;
+reg                 ram_his_wr_b;
 
-reg ram_accu_wr;
-reg [7:0] ram_accu_wraddr;
-wire [18:0] ram_accu_wrdata;
+reg                 ram_accu_wr;
+reg     [7:0]       ram_accu_wraddr;
+wire    [18:0]      ram_accu_wrdata;
 
-wire [18:0] ram_accu_rddata;
+wire    [18:0]      ram_accu_rddata;
 
-/////////////////////////////////////////
-//    直方图统计
-/////////////////////////////////////////
+
+//************************ Main Code ************************//
+
+
+//************************ 直方图统计 ************************//
 
 //摄像头输入的灰度数据有效时，会先进行读RAM操作，然后将读出的数据加1后，再次写入相同地址
 //读操作需要一个时钟周期，因此“写使能”直接将输入的Valid信号延迟一个时钟周期即可
@@ -45,28 +51,29 @@ always @(posedge clk or negedge rst_n) begin
     if(!rst_n)
         ram_his_wr_en <= 1'b0;
     else
-        ram_his_wr_en <= per_frame_clken;
+        ram_his_wr_en <= per_frame_de;
 end
 
 //双端口RAM，用于统计一帧图像的灰度直方图
-ram_2port_his u_ram_2port_his(
+ram_2port_his       ram_2port_his_inst
+(
     //端口A：摄像头输出的灰度数据作为RAM地址，先将该地址中的数据读出来，加1后再写入RAM1中
-    .clock_a(clk),
-    .aclr_a(!rst_n),
-    .rden_a(per_frame_clken),  //读端口A
-    .address_a(per_img_Y),
-    .q_a(ram_his_rd_data),
-    .wren_a(ram_his_wr_en),  //写端口A
-    .data_a(ram_his_rd_data+1),  //直方图统计过程中，写入RAM的数据为（当前地址的数据+1）
+    .clock_a    (clk),
+    .aclr_a     (!rst_n),
+    .rden_a     (per_frame_de),  //读端口A
+    .address_a  (per_img_Y),
+    .q_a        (ram_his_rd_data),
+    .wren_a     (ram_his_wr_en),  //写端口A
+    .data_a     (ram_his_rd_data+1),  //直方图统计过程中，写入RAM的数据为（当前地址的数据+1）
 
     //端口B：一帧结束后读出RAM中的数据进行寄存，下一帧开始前将RAM初始化为0
-    .clock_b(clk),
-    .aclr_b(1'b0),
-    .rden_b(ram_his_rd_b),  //读端口B
-    .address_b(ram_his_addr_b),
-    .q_b(ram_his_rd_data_b),
-    .wren_b(ram_his_wr_b),  //写端口B
-    .data_b(32'd0)  //初始化过程中，写入RAM的数据为0
+    .clock_b    (clk),
+    .aclr_b     (1'b0),
+    .rden_b     (ram_his_rd_b),  //读端口B
+    .address_b  (ram_his_addr_b),
+    .q_b        (ram_his_rd_data_b),
+    .wren_b     (ram_his_wr_b),  //写端口B
+    .data_b     (19'd0)  //初始化过程中，写入RAM的数据为0
 );
 
 //采集摄像头场同步信号上升/下降沿
@@ -130,7 +137,7 @@ always @(posedge clk or negedge rst_n) begin
     if (!rst_n)
         ram_accu_wr <= 1'b0;
     else
-        ram_accu_wr <= ram_his_rd_b;
+        ram_accu_wr <= ram_his_rd_b;//读完再写
 end
 
 reg [18:0] his_sum; //累积直方图结果
@@ -147,7 +154,29 @@ always @(posedge clk or negedge rst_n) begin
     end
 end
 
-assign ram_accu_wrdata = his_sum + ram_his_rd_data_b;
+assign ram_accu_wrdata = his_sum + ram_his_rd_data_b;//不能直接取ram_his_rd_data_b
+
+
+//将直方图统计信息寄存到VGA显存RAM
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n)
+        ram_accu_wraddr <= 8'd0;
+    else
+        ram_accu_wraddr <= ram_his_addr_b;
+end
+
+//累积直方图RAM，用于在每一帧图像的直方图信息统计完毕后，累积统计结果，该结果会用于直方图均衡
+ram_2port_accu ram_2port_accu_inst
+(
+    .clock      (clk),
+    .aclr       (!rst_n),
+    .wren       (ram_accu_wr),
+    .wraddress  (ram_accu_wraddr),
+    .data       (ram_accu_wrdata), //写入的数据是从前一个RAM端口B读出的数据 + 累积结果
+    .rden       (per_frame_de), //摄像头输入图像时，使用上一帧的直方图统计信息
+    .rdaddress  (per_img_Y),
+    .q          (ram_accu_rddata)
+);
 
 
 //////////////////////////////////////////
@@ -176,23 +205,26 @@ end
 //延迟三个时钟周期以达到与数据同步
 reg [2:0] per_frame_vsync_r;
 reg [2:0] per_frame_href_r;
-reg [2:0] per_frame_clken_r;
+reg [2:0] per_frame_de_r;
 
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
         per_frame_vsync_r <= 0;
         per_frame_href_r <= 0;
-        per_frame_clken_r <= 0;
+        per_frame_de_r <= 0;
     end else begin
         per_frame_vsync_r <= {per_frame_vsync_r[1:0], per_frame_vsync};
         per_frame_href_r <= {per_frame_href_r[1:0], per_frame_href};
-        per_frame_clken_r <= {per_frame_clken_r[1:0], per_frame_clken};
+        per_frame_de_r <= {per_frame_de_r[1:0], per_frame_de};
     end
 end
 
 assign post_frame_vsync = per_frame_vsync_r[2];
 assign post_frame_href = per_frame_href_r[2];
-assign post_frame_clken = per_frame_clken_r[2];
+assign post_frame_clken = per_frame_de_r[2];
 assign post_img_Y = post_frame_href ? data_div : 8'd0;
 
+
+
 endmodule
+
